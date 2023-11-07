d5e09f6acc25fe718378e4176b050440eb132309
Joshuinn <delmonteo.joshua@gmail.com>
2023-11-06 22:34:13 -0600

    Subiendo archivos

--- src/app/pages/assistence/page.jsx
+++ src/app/pages/assistence/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ListGroup from "@/app/components/assistance/ListGroup";
 import Header from "@/app/components/Header";
 
+
 function page() {
   return (
     <div className="h-screen p-3 rounded-lg">
