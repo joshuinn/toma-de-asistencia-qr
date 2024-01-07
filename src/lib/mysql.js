5288e8a7d41e30b02785fbb7aaccf65ab30e1ff2
joshuinn <delmonteo.joshua@gmail.com>
2024-01-06 18:14:37 -0600

    moviendo credenciales de db  a .env

--- src/lib/mysql.js
+++ src/lib/mysql.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 export const conn = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  port: 3306,
+  host: process.env.HOST_DB,
+  user: process.env.USER_DB,
+  password: process.env.PASSWORD_DB,
+  port: process.env.PORT_DB,
   database: process.env.DATABASE,
 });
 
