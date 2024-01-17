beefdb2b68a77ab1e112ffc77f5c7f8049aa851f
joshuinn <delmonteo.joshua@gmail.com>
2024-01-16 18:31:15 -0600

    cambiando formato de fecha

--- src/app/api/listReports/route.js
+++ src/app/api/listReports/route.js
@@ -1,6 +1,21 @@
 import { conn } from "@/lib/mysql";
 import { NextResponse } from "next/server";
 
+function cambiarFormatoFecha(fecha) {
+  // Dividir la fecha en día, mes y año
+  var partesFecha = fecha.split("/");
+
+  // Obtener el día, mes y año
+  var dia = partesFecha[0];
+  var mes = partesFecha[1];
+  var anio = partesFecha[2];
+
+  // Reconstruir la fecha en el formato deseado
+  var nuevaFecha = anio + "/" + mes + "/" + dia;
+
+  return nuevaFecha;
+}
+
 export async function POST(req) {
   try {
     const data = await req.json();
@@ -22,7 +37,8 @@ export async function POST(req) {
       " JOIN `ctb_lista_asistencia` ON ttb_asistencia.id_lista_asistencia = ctb_lista_asistencia.id_lista_asistencia ";
     let fecha_min = JSON.stringify(data.fecha_min);
     let fecha_max = JSON.stringify(data.fecha_max);
-    
+    fecha_min = cambiarFormatoFecha(fecha_min);
+    fecha_max = cambiarFormatoFecha(fecha_max);
     if (fecha_min.length == 2 || fecha_max.length == 2) {
       fecha_min = fecha_min.length == 0 ? fecha_max : fecha_min;
       fecha_max = fecha_min;
@@ -46,9 +62,12 @@ export async function POST(req) {
         : " WHERE ttb_asistencia.id_lista_asistencia =  ";
     for (let i = 0; i < data.list.length; i++) {
       let dataResponse = await conn.query(
-        bodyQuery + bodyWhere + data.list[i].id_lista_asistencia + " ORDER BY 16 ASC"
+        bodyQuery +
+          bodyWhere +
+          data.list[i].id_lista_asistencia +
+          " ORDER BY 16 ASC"
       );
-      newData[i] = dataResponse[0]
+      newData[i] = dataResponse[0];
     }
     return NextResponse.json(newData);
   } catch (error) {
